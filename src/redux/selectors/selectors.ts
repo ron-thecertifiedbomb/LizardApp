de5f48dc9c899a66c1111f7a996e5d060e78b458
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2024-05-18 16:45:00 +0800

    added selectors

--- src/redux/selectors/selectors.ts
+++ src/redux/selectors/selectors.ts
@@ -6,5 +6,4 @@ export const selectUserId = (state: RootState) => state.userId.userId;
 export const selectDataLoading = (state: RootState) => state.data.isLoading;
 export const selectDataError = (state: RootState) => state.data.isError;
 export const selectData = (state: RootState) => state.data.data;
-
 export const selectFilteredData = (state: RootState) => state.data.filteredData;
