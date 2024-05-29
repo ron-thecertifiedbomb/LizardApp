0058a0c44abcca171b4564a5853443fc34f707b2
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2024-05-29 09:28:21 +0800

    sadasd

--- src/screen/LogInScreen.tsx
+++ src/screen/LogInScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {View} from 'react-native';
 import LogInForm from '../components/authentication/loginform/LogInForm';
+import LogIn from '../components/authentication/loginform/LogIn';
 
 export default function LogInScreen() {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <LogInForm />
+
     </View>
   );
 }
